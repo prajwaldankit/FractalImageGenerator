ff30d5c7b7fffa37ca804d69d470433585bc6d03
prajwaldankit <prajwal.x82@gmail.com>
2019-12-11 14:54:51 +0545

    finished mandelbrot set with zooming feature

--- assets/js/FractalCanvas.js
+++ assets/js/FractalCanvas.js
@@ -1,6 +1,16 @@
 const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 480;
+const CANVAS_HEIGHT = 600;
+/**
+ *
+ * Generates the canvas inside the div for drawing the fractal images
+ * @class FractalCanvas
+ */
 class FractalCanvas {
+  /**
+   *Creates an instance of FractalCanvas.
+   * @param {*} parent
+   * @memberof FractalCanvas
+   */
   constructor(parent) {
     this.parent = parent;
     this.init();
@@ -8,16 +18,26 @@ class FractalCanvas {
     new Madelbrot(this.canvas);
   }
 
+  /**
+   *
+   *
+   * @memberof FractalCanvas
+   */
   init() {
     this.canvas = document.createElement('canvas');
     this.context = this.canvas.getContext('2d');
     this.applyStyle();
     this.parent.appendChild(this.canvas);
   }
+  /**
+   *
+   *apply stye to the canvas
+   * @memberof FractalCanvas
+   */
   applyStyle() {
     this.canvas.width = CANVAS_WIDTH;
     this.canvas.height = CANVAS_HEIGHT;
-    this.canvas.style.background = '#262626';
+    this.canvas.style.background = '#000';
   }
   draw() {
   }
--- assets/js/Mandelbrot.js
+++ assets/js/Mandelbrot.js
@@ -1,45 +1,56 @@
 class Madelbrot {
+
   constructor(parent) {
-    this.magnificationFactor = 900;
-    this.panX = 0;
-    this.panY = 0;
+    this.magnificationFactor = 200;
+    this.panX = .5;
+    this.panY = .6;
     this.canvas = parent;
+    let that = this;
     this.context = this.canvas.getContext('2d');
     this.init();
+    window.addEventListener('keypress', () => {
+      that.magnificationFactor += 400;
+      console.log(that.magnificationFactor)
+      that.context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+      this.init(this.noOfIterations + 10)
+    })
   }
-  init() {
+
+  init(noOfIterations) {
+    this.noOfIterations = noOfIterations || 50;
     for (let x = 0; x < this.canvas.width; x++) {
       for (let y = 0; y < this.canvas.height; y++) {
         let belongsToSet =
           this.checkIfBelongsToMandelbrotSet(x / this.magnificationFactor - this.panX,
             y / this.magnificationFactor - this.panY);
-        if (belongsToSet) {
+        if (belongsToSet != 0) {
+          this.context.fillStyle = `hsl(240,100%,${belongsToSet}%)`;
           this.context.fillRect(x, y, 1, 1); // Draw a black pixel
         }
       }
     }
   }
-  checkIfBelongsToMandelbrotSet(x, y) {
-    let realComponentOfResult = x;
-    let imaginaryComponentOfResult = y;
 
-    for (var i = 0; i < 10; i++) {
+  checkIfBelongsToMandelbrotSet(x, y) {
+    let realComponent = x;
+    let imaginaryComponent = y;
+    let maxIterations = this.noOfIterations;
+    for (var i = 0; i < maxIterations; i++) {
       // Calculate the real and imaginary components of the result
       // separately
-      let tempRealComponent = realComponentOfResult * realComponentOfResult
-        - imaginaryComponentOfResult * imaginaryComponentOfResult
+      let tempRealComponent = realComponent * realComponent
+        - imaginaryComponent * imaginaryComponent
         + x;
 
-      let tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult
+      let tempImaginaryComponent = 2 * realComponent * imaginaryComponent
         + y;
 
-      realComponentOfResult = tempRealComponent;
-      imaginaryComponentOfResult = tempImaginaryComponent;
+      realComponent = tempRealComponent;
+      imaginaryComponent = tempImaginaryComponent;
+      if (realComponent * imaginaryComponent > 5)
+        return (i / maxIterations * 100); // In the Mandelbrot set
     }
-
-    if (realComponentOfResult * imaginaryComponentOfResult < 5)
-      return true; // In the Mandelbrot set
-
-    return false;
+    return 0;
   }
+
 }
